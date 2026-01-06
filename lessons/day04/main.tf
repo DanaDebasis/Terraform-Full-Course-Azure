9f8fba58af54ecc8b50f9cfdd02e90260ec98a92
DanaDebasis <128150130+DanaDebasis@users.noreply.github.com>
2026-01-06 12:55:09 +0530

    Update Azure backend configuration in main.tf

--- lessons/day04/main.tf
+++ lessons/day04/main.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tfstate-day04"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "day0417691"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    resource_group_name  = "deb-terraform-azure-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "debterraformstorage1"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
@@ -18,17 +18,18 @@ provider "azurerm" {
     features {
       
     }
-  
+  subscription_id = "ca62517a-b533-4891-8cb5-db5528753f34"
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
-  location = "West Europe"
+  //location = "West Europe"
+  location = "Central India"
 }
 
 resource "azurerm_storage_account" "example" {
  
-  name                     = "techtutorial101"
+  name                     = "debterraformstorage101"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location # implicit dependency
   account_tier             = "Standard"
